ee29b3b6fe2cb458862ae588eacaedf0669c65be
Eugene <makarovevgeniy7a@inbox.ru>
2014-04-29 11:36:21 -0700

    Update MainForm.cs

--- Fathers/MainForm.cs
+++ Fathers/MainForm.cs
@@ -63,6 +63,7 @@ namespace Practice
             TxT.n = Int32.Parse(SupportStringArray[7]);
             return TxT;
         }
+        
         private void DrawGraph(double[] ArrayArray)
         {
             int i = 0;
@@ -75,6 +76,58 @@ namespace Practice
                 PairList.Add(x, ArrayArray[Convert.ToInt32(100 * x)]);
             i++;
             LineItem myCurve = Pane.AddCurve("White noise", PairList, Color.Blue, SymbolType.None);
+            zedGraph.AxisChange();
+            zedGraph.Refresh();
+        }
+        private void DrawGraph(double[,] ArrayArray)
+        {
+            int i = 0;
+            GraphPane Pane = zedGraph.GraphPane;
+            Pane.CurveList.Clear();
+            PointPairList PairList = new PointPairList();
+            PointPairList PairList_2 = new PointPairList();
+            PointPairList PairList_3 = new PointPairList();
+            double xmin = 0;
+            double xmax = 3;
+            for (double x = xmin; x <= xmax; x += 0.01)
+            {
+                PairList.Add(x, ArrayArray[0, Convert.ToInt32(100 * x)]);
+                PairList_2.Add(x, ArrayArray[1, Convert.ToInt32(100 * x)]);
+                PairList_3.Add(x, ArrayArray[2, Convert.ToInt32(100 * x)]);
+            }
+            i++;
+            LineItem myCurve = Pane.AddCurve("Factor = 0.2", PairList, Color.Black, SymbolType.None);
+            LineItem myCurve_2 = Pane.AddCurve("Factor = 0.5", PairList_2, Color.Blue, SymbolType.None);
+            LineItem myCurve_3 = Pane.AddCurve("Factor = 0.8", PairList_3, Color.Red, SymbolType.None);
+            zedGraph.AxisChange();
+            zedGraph.Refresh();
+        }
+        private void DrawGraph(double[,] ArrayArray1, double[,] ArrayArray2, int LineNumber) 
+        {
+            int i = 0;
+            GraphPane Pane = zedGraph.GraphPane;
+            Pane.CurveList.Clear();
+            int axis2;
+            axis2 = Pane.AddYAxis("Ось графика Х");
+            PointPairList PairList = new PointPairList();
+            PointPairList PairList_2 = new PointPairList();
+            double xmin = 0;
+            double xmax = 49;
+            for (double x = xmin; x <= xmax; x += 0.01)
+            {
+                PairList.Add(x, ArrayArray1[LineNumber, Convert.ToInt32(100 * x)]);
+                PairList_2.Add(x, ArrayArray2[LineNumber, Convert.ToInt32(100 * x)]);
+            }
+            i++;
+            LineItem myCurve = Pane.AddCurve("X", PairList, Color.Black, SymbolType.None);
+            LineItem myCurve_2 = Pane.AddCurve("Y1", PairList_2, Color.Blue, SymbolType.None);
+            myCurve_2.YAxisIndex = axis2;
+            Pane.YAxis.IsVisible = false;
+            Pane.YAxisList[axis2].Title.FontSpec.FontColor = Color.Blue;
+            if (!isAxisAdded)
+                Pane.YAxisList[axis2].IsVisible = true;
+            else
+                Pane.YAxisList[axis2].IsVisible = false;
             zedGraph.AxisChange();
             zedGraph.Refresh();
         }
